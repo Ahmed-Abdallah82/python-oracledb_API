80ea99a07b2e34fdbc1f0410283c41c6feb90198
Ahmed Abdallah <126517542+Ahmed-Abdallah82@users.noreply.github.com>
2023-04-22 12:50:23 +0200

    commit

--- select.sql
+++ select.sql
@@ -4,3 +4,17 @@ on (e.department_id = d.department_id)
 join jobs j
 on (e.job_id = j.job_id)
 order by e.salary desc, j.job_title, d.department_name;
+
+--pivot tables are equal to these selects:
+
+select j.job_title, sum(e.salary)
+from employees e join jobs j
+on (e.job_id = j.job_id)
+group by j.job_title
+order by sum(e.salary);
+
+select d.department_name, sum(e.salary)
+from employees e left outer join departments d
+on (e.department_id = d.department_id)
+group by d.department_name
+order by sum(e.salary);
